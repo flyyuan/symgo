dacc482976ac2a79bc9235a1c55dcd289b7fab9d
flyyuan <740004544@qq.com>
2023-08-13 17:48:59 +0800

    feat: init project

---
+++ symgo.go
@@ -0,0 +1,11 @@
+/*
+ * @Author: flyyuan 740004544@qq.com
+ * @Date: 2023-08-13 17:48:26
+ * @LastEditors: flyyuan 740004544@qq.com
+ * @LastEditTime: 2023-08-13 17:48:46
+ * @FilePath: /symgo/symgo.go
+ * @Description:
+ */
+package symgo
+
+// Exported symbols, functions, types, etc.
